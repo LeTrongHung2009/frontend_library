bf79cf36fa81b30ed8a7de2098c22707fbbd4bd1
LeTrongHung2009 <hung5b2000@gmail.com>
2025-12-06 20:25:56 +0700

    Update Login.jsx

--- client/src/pages/auth/Login.jsx
+++ client/src/pages/auth/Login.jsx
@@ -14,7 +14,7 @@ const Login = () => {
 
     try {
       // 1. Gọi API Login
-      const res = await axios.post('http://library.library-os.workers.dev/api/auth/login', formData);
+      const res = await axios.post('https://library.library-os.workers.dev/api/auth/login', formData);
       const { token, user } = res.data;
 
       // 2. Kiểm tra token (quan trọng)
